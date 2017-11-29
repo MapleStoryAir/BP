cf5680c5fbc848bcff6e0765cae87e9a353e4b25
zhuwenguang <645427043@qq.com>
2017-11-29 13:23:27 +0800

    update url

--- BP/urls.py
+++ BP/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^adminzhu/', admin.site.urls),
-    url(r'^xxx/', include("blog.urls", namespace='blog')),
+    url(r'^/', include("blog.urls", namespace='blog')),
     url(r'^comments/', include("comments.urls", namespace='comments')),
 ]
